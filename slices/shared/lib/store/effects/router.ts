77e5d16adc44cb633b24e02c880953733559ed64
Vladimir Yazykov <neizerth@gmail.com>
2025-03-11 20:55:12 +0300

    removed log

--- slices/shared/lib/store/effects/router.ts
+++ slices/shared/lib/store/effects/router.ts
@@ -2,7 +2,6 @@ import { type Href, router } from "expo-router";
 
 export const goToPage = (href: Href) => () => {
   router.push(href);
-  console.log('goto', href);
 }
 
 export const replacePageTo = (href: Href) => () => {
