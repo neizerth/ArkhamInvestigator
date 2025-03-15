eb1016b290c6649f93ada474758f8549e6734689
Vladimir Yazykov <neizerth@gmail.com>
2025-03-16 00:17:11 +0300

    fixed px value

--- slices/shared/ui/content/typography/Text.tsx
+++ slices/shared/ui/content/typography/Text.tsx
@@ -6,7 +6,7 @@ import styled from "styled-components/native";
 export const Text: typeof NativeText = styled(NativeText)`
   color: ${color.light10};
   font-family: ${Alegreya.regular};
-  font-size: ${font.size.default};
+  font-size: ${font.size.default}px;
 `
 
 export const Bold: typeof Text = styled(Text)`
