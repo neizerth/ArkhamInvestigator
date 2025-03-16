47df4245c923245f3db9ede97fe8872bf6f19ac8
Vladimir Yazykov <neizerth@gmail.com>
2025-03-16 15:05:55 +0300

    updated new game colors

--- slices/pages/new-game/ui/NewGamePage/NewGamePage.components.tsx
+++ slices/pages/new-game/ui/NewGamePage/NewGamePage.components.tsx
@@ -4,5 +4,5 @@ import { View } from "react-native";
 import styled from "styled-components/native";
 
 export const Page: typeof ContentPage = styled(ContentPage)`
-
+  background-color: ${color.dark30};
 `
