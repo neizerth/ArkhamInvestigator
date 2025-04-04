82019f3d8d59ad839e5e6ad4a9de5dff39cc5ab1
Vladimir Yazykov <neizerth@gmail.com>
2025-04-04 19:50:55 +0300

    fix: select styles

--- slices/widgets/control/store-select/ui/StoreSelect/StoreSelect.components.ts
+++ slices/widgets/control/store-select/ui/StoreSelect/StoreSelect.components.ts
@@ -6,7 +6,7 @@ import styled from "styled-components/native";
 
 export const Container: typeof Row = styled(Row)`
   position: relative;
-  padding-top: ${size.gap.medium}px;
+  padding-top: 20px;
   background-color: ${color.dark30};
   border-radius: ${size.borderRadius.default}px;
 `;
@@ -14,7 +14,7 @@ export const Container: typeof Row = styled(Row)`
 export const Select: typeof HapticSelect = styled(HapticSelect).attrs({
 	iconStyle: {
 		position: "relative",
-		top: -size.gap.small,
+		top: -size.gap.default + 2,
 	},
 })`
   flex: 1;
@@ -25,7 +25,7 @@ export const Label: typeof Text = styled(Text)`
   font-family: ${Alegreya.italic};
   position: absolute;
   z-index: 1;
-  left: ${size.gap.small}px;
-  top: ${size.gap.small}px;
-  font-size: ${font.size.small};
+  left: ${size.gap.small + 1}px;
+  top: ${size.gap.default}px;
+  font-size: ${font.size.small}px;
 `;
