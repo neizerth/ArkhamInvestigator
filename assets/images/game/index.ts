e4b58564145c879a700464d4eb8af32509e3cf84
Vladimir Yazykov <neizerth@gmail.com>
2025-04-18 00:46:09 +0300

    fix: speed up loading

--- assets/images/game/index.ts
+++ assets/images/game/index.ts
@@ -1,15 +1,7 @@
 import description from "./description";
-import effects from "./effects";
 import faction from "./faction";
 import skills from "./skills";
 import stats from "./stats";
 import title from "./title";
 
-export default [
-	...description,
-	...faction,
-	...skills,
-	...title,
-	...stats,
-	...effects,
-];
+export default [...description, ...faction, ...skills, ...title, ...stats];
