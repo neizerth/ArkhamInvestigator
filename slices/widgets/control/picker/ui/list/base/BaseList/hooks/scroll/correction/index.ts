4763edbdb64aa62022cd9aa4fa878b0a7fa547a9
Vladimir Yazykov <neizerth@gmail.com>
2025-04-20 20:30:11 +0300

    fix: ios overscroll back

--- slices/widgets/control/picker/ui/list/base/BaseList/hooks/scroll/correction/index.ts
+++ slices/widgets/control/picker/ui/list/base/BaseList/hooks/scroll/correction/index.ts
@@ -1,7 +1,3 @@
-import { identity, pipe } from "ramda";
-import { Platform } from "react-native";
 import { useScrollBack } from "./useScrollBack";
 
-const useAndroidCorrection = pipe(useScrollBack);
-
-export default Platform.OS === "android" ? useAndroidCorrection : identity;
+export default useScrollBack;
