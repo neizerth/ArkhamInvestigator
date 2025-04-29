ba742988037a59bb708f5bb06add95af9b4fb8d6
Vladimir Yazykov <neizerth@gmail.com>
2025-04-29 12:51:26 +0300

    fix: removed showing info timeout

--- slices/pages/board/board/ui/shared/features/AdditionalInfoArea/AdditionalInfoArea.tsx
+++ slices/pages/board/board/ui/shared/features/AdditionalInfoArea/AdditionalInfoArea.tsx
@@ -1,30 +1,27 @@
 import { setShowRevealChaosTokenModal } from "@features/chaos-bag";
 import type { PressableProps } from "@features/haptic";
 import { setShowAdditionalInformation, useAppDispatch } from "@shared/lib";
-import { useCallback, useRef } from "react";
+import { useCallback } from "react";
 import * as C from "./AdditionalInfoArea.components";
 
 export type AdditionalInfoAreaProps = PressableProps;
 
 export const AdditionalInfoArea = (props: AdditionalInfoAreaProps) => {
 	const dispatch = useAppDispatch();
-	const timeout = useRef<NodeJS.Timeout>();
 
-	const onShow = useCallback(() => {
-		dispatch(setShowRevealChaosTokenModal(true));
-		dispatch(setShowAdditionalInformation(true));
+	const handlePress = useCallback(
+		(value: boolean) => () => {
+			dispatch(setShowRevealChaosTokenModal(value));
+			dispatch(setShowAdditionalInformation(value));
+		},
+		[dispatch],
+	);
 
-		clearTimeout(timeout.current);
-
-		timeout.current = setTimeout(() => {
-			dispatch(setShowAdditionalInformation(false));
-		}, 1000);
-	}, [dispatch]);
-
-	const onHide = useCallback(() => {
-		dispatch(setShowRevealChaosTokenModal(false));
-		dispatch(setShowAdditionalInformation(false));
-	}, [dispatch]);
-
-	return <C.Container {...props} onPressIn={onShow} onPressOut={onHide} />;
+	return (
+		<C.Container
+			{...props}
+			onPressIn={handlePress(true)}
+			onPressOut={handlePress(false)}
+		/>
+	);
 };
