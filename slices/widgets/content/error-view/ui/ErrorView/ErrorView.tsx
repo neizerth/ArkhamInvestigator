0486f6847ca9d3995e9708b94f31765d4e3dd239
Vladimir Yazykov <neizerth@gmail.com>
2025-05-11 08:14:17 +0300

    fix: removed console.log

--- slices/widgets/content/error-view/ui/ErrorView/ErrorView.tsx
+++ slices/widgets/content/error-view/ui/ErrorView/ErrorView.tsx
@@ -8,8 +8,6 @@ export type ErrorViewProps = {
 
 export const ErrorView = ({ error }: ErrorViewProps) => {
 	const { t } = useAppTranslation();
-
-	console.log({ error });
 	return (
 		<C.Container>
 			<Title>{t`Error`}</Title>
