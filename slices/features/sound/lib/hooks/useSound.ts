87b73add5325a76d369cc84855d8474b371b708c
Vladimir Yazykov <neizerth@gmail.com>
2025-05-12 07:50:58 +0300

    fix: hide sound settings

--- slices/features/sound/lib/hooks/useSound.ts
+++ slices/features/sound/lib/hooks/useSound.ts
@@ -4,6 +4,8 @@ import type { SoundId } from "../../model";
 import { selectSoundEnabled } from "../store";
 import { useSoundPlayer } from "./useSoundPlayer";
 
+const SOUND_DISABLED = true;
+
 export const useSound = (id?: SoundId) => {
 	const soundEnabled = useAppSelector(selectSoundEnabled);
 
@@ -11,7 +13,7 @@ export const useSound = (id?: SoundId) => {
 
 	return useCallback(
 		(enabled = true) => {
-			if (!soundEnabled || !enabled) {
+			if (!soundEnabled || !enabled || SOUND_DISABLED) {
 				return;
 			}
 			if (player.currentTime !== 0) {
