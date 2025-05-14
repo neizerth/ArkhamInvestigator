24a0bc9e9647e9062ebe6a077de8ec09a973e8ee
Vladimir Yazykov <neizerth@gmail.com>
2025-05-14 22:07:39 +0300

    fix: full view view navbar

--- slices/shared/ui/behavior/FullHeightView/FullHeightView.tsx
+++ slices/shared/ui/behavior/FullHeightView/FullHeightView.tsx
@@ -1,11 +1,12 @@
 import { Dimensions, View, type ViewProps } from "react-native";
+import { navBarHeight } from "../../../config";
 export type FullHeightViewProps = ViewProps;
 
 const { height } = Dimensions.get("screen");
 
 export const FullHeightView = (props: FullHeightViewProps) => {
 	const style = {
-		minHeight: height,
+		minHeight: height - navBarHeight,
 	};
 
 	return <View {...props} style={[props.style, style]} />;
