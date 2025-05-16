c8745cbdd134420936df63c86991e32a3dec9e11
Vladimir Yazykov <neizerth@gmail.com>
2025-05-16 13:18:45 +0300

    fix: clear expression after closing modal

--- slices/features/chaos-bag/lib/store/features/chaosBag/actions/token/reveal/modal/closeRevealChaosTokenModal.ts
+++ slices/features/chaos-bag/lib/store/features/chaosBag/actions/token/reveal/modal/closeRevealChaosTokenModal.ts
@@ -1,5 +1,6 @@
 import type { AppThunk } from "@shared/model";
 import {
+	setChaosBagSkillCheckExpression,
 	setChaosBagSkillCheckTitle,
 	setChaosBagSkillCheckType,
 	setChaosBagSkillValue,
@@ -11,6 +12,7 @@ import { addRevealHistoryItem } from "../../../history";
 export const closeRevealChaosTokenModal = (): AppThunk => (dispatch) => {
 	dispatch(addRevealHistoryItem());
 	dispatch(setRevealedTokenIds([]));
+	dispatch(setChaosBagSkillCheckExpression([]));
 	dispatch(setChaosBagSkillValue(null));
 	dispatch(setChaosBagSkillCheckType(null));
 	dispatch(setChaosBagSkillCheckTitle(null));
