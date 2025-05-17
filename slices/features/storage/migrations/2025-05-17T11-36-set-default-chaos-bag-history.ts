eeecf6cc3569b7f0232c5db6327f13c8a5dafab9
Vladimir Yazykov <neizerth@gmail.com>
2025-05-17 11:42:01 +0300

    fix: initial chaos bag history state

---
+++ slices/features/storage/migrations/2025-05-17T11-36-set-default-chaos-bag-history.ts
@@ -0,0 +1,21 @@
+import type { PersistedState } from "redux-persist";
+import type { IChaosBagState } from "../../chaos-bag";
+
+type State = PersistedState & {
+	chaosBag?: IChaosBagState;
+};
+
+export default function v10(state?: State) {
+	if (!state?.chaosBag) {
+		return;
+	}
+
+	return {
+		...state,
+		chaosBag: {
+			...state.chaosBag,
+			skillCheckExpression: state.chaosBag.skillCheckExpression || [],
+			revealHistory: state.chaosBag.revealHistory || [],
+		},
+	};
+}
