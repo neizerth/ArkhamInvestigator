d029a69ef193eb5becfbff9a2d33a7b8e51b21c6
Vladimir Yazykov <neizerth@gmail.com>
2025-06-01 13:09:12 +0300

    feat: rules background title

--- slices/features/game/rules/ui/RulesBackground/RulesBackground.components.tsx
+++ slices/features/game/rules/ui/RulesBackground/RulesBackground.components.tsx
@@ -1,6 +1,10 @@
+import { Arkhamic, Conkordia, STXinwei, SanCn } from "@assets/fonts";
 import { roundReferenceAssets } from "@assets/images/game/reference/round";
+import { color } from "@shared/config";
 import { ImageBackground } from "@shared/ui";
+import { View } from "react-native";
 import styled from "styled-components/native";
+import { withLocale } from "../../../../i18n";
 import { currentRoundReferenceSize } from "../../config";
 
 export const Container: typeof ImageBackground = styled(ImageBackground).attrs({
@@ -10,3 +14,44 @@ export const Container: typeof ImageBackground = styled(ImageBackground).attrs({
   height: ${currentRoundReferenceSize.height}px;
   padding: 6% 5% 8%;
 `;
+
+export const Content: typeof View = styled(View)`
+  flex: 1;
+`;
+
+export const Title: typeof View = styled(View)`
+  position: absolute;
+	bottom: 2.9%;
+	left: 0;
+	right: 0;
+	align-items: center;
+`;
+
+const titleFontSize = Math.round((currentRoundReferenceSize.width * 22) / 1000);
+
+export const TitleText = withLocale({
+	style: {
+		default: {
+			fontFamily: Arkhamic.regular,
+			fontSize: titleFontSize,
+			color: color.black,
+			paddingLeft: titleFontSize * 1.5,
+			textTransform: "uppercase",
+			textAlign: "center",
+		},
+		ru: {
+			fontSize: titleFontSize,
+			fontFamily: Conkordia.regular,
+			paddingLeft: titleFontSize * 1.2,
+		},
+		ko: {
+			fontFamily: SanCn.bold,
+		},
+		zh: {
+			fontFamily: STXinwei.regular,
+		},
+		"zh-cn": {
+			fontFamily: STXinwei.regular,
+		},
+	},
+});
--- slices/features/game/rules/ui/RulesBackground/RulesBackground.tsx
+++ slices/features/game/rules/ui/RulesBackground/RulesBackground.tsx
@@ -1,8 +1,20 @@
 import type { ImageBackgroundProps } from "@shared/ui";
+import { useAppTranslation } from "../../../../i18n";
 import * as C from "./RulesBackground.components";
 
 export type RulesBackgroundProps = ImageBackgroundProps;
 
-export const RulesBackground = (props: RulesBackgroundProps) => {
-	return <C.Container {...props} />;
+export const RulesBackground = ({
+	children,
+	...props
+}: RulesBackgroundProps) => {
+	const { t } = useAppTranslation();
+	return (
+		<C.Container {...props}>
+			<C.Content>{children}</C.Content>
+			<C.Title>
+				<C.TitleText>{t`Rules Reference`}</C.TitleText>
+			</C.Title>
+		</C.Container>
+	);
 };
