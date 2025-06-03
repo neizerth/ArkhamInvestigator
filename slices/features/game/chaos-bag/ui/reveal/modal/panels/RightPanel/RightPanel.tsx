18e42be6b997a6f960eacf05972e5492e0f91350
Vladimir Yazykov <neizerth@gmail.com>
2025-06-03 15:46:02 +0300

    fix: remove skill result

--- slices/features/game/chaos-bag/ui/reveal/modal/panels/RightPanel/RightPanel.tsx
+++ slices/features/game/chaos-bag/ui/reveal/modal/panels/RightPanel/RightPanel.tsx
@@ -8,16 +8,13 @@ import {
 } from "@shared/lib";
 import { useCallback } from "react";
 import type { ViewProps } from "react-native";
-import { selectSkillCheckResult } from "../../../../../lib";
 import * as C from "./RightPanel.components";
 
 export type RightPanelProps = ViewProps;
 
-const showResult = false;
 export const RightPanel = ({ ...props }: RightPanelProps) => {
 	const dispatch = useAppDispatch();
 	const difficulty = useAppSelector(selectSkillCheckDifficulty);
-	const result = useAppSelector(selectSkillCheckResult);
 
 	const dificultyCharacter = useAppSelector(
 		selectSkillCheckDifficultyCharacter,
@@ -47,16 +44,6 @@ export const RightPanel = ({ ...props }: RightPanelProps) => {
 						</>
 					)}
 				</C.Item>
-				{showResult && (
-					<C.Item>
-						{typeof result === "number" && (
-							<>
-								<C.ResultSymbol>=</C.ResultSymbol>
-								<C.Value value={result} />
-							</>
-						)}
-					</C.Item>
-				)}
 			</C.Actions>
 		</C.Container>
 	);
