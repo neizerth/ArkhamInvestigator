ef5b266bdab9148ac0c77ef010b896946f5748e3
Vladimir Yazykov <neizerth@gmail.com>
2025-06-08 14:07:18 +0300

    fix: add skill value changes to history

--- slices/features/game/chaos-bag/lib/store/features/chaosBag/actions/token/reveal/updateChaosBagSkillValue.ts
+++ slices/features/game/chaos-bag/lib/store/features/chaosBag/actions/token/reveal/updateChaosBagSkillValue.ts
@@ -1,14 +1,30 @@
 import type { AppThunk } from "@shared/model";
 import {
+	selectRevealHistoryItem,
 	setChaosBagSkillCheckExpression,
 	setChaosBagSkillCheckTitle,
 	setChaosBagSkillValue,
+	setRevealHistoryItem,
 } from "../../../chaosBag";
 
 export const updateChaosBagSkillValue =
 	(value: number): AppThunk =>
-	(dispatch) => {
+	(dispatch, getState) => {
+		const state = getState();
 		dispatch(setChaosBagSkillCheckExpression([]));
 		dispatch(setChaosBagSkillCheckTitle(null));
 		dispatch(setChaosBagSkillValue(value));
+
+		const historyItem = selectRevealHistoryItem(state);
+
+		if (!historyItem) {
+			return;
+		}
+
+		dispatch(
+			setRevealHistoryItem({
+				...historyItem,
+				skillCheckValue: value,
+			}),
+		);
 	};
