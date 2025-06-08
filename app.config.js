e6a0f3ce21b19d85ffc349a816b1162e9bcc68ca
Vladimir Yazykov <neizerth@gmail.com>
2025-06-08 08:05:42 +0300

    build: 1.33.0

--- app.config.js
+++ app.config.js
@@ -33,7 +33,7 @@ module.exports = {
         backgroundColor: "#000",
       },
       package: packageId,
-      versionCode: 313,
+      versionCode: 315,
     },
     web: {
       bundler: "metro",
