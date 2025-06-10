0aa74c52c585ee1fa5c9991a7823fab59d931783
Vladimir Yazykov <neizerth@gmail.com>
2025-06-10 15:13:44 +0300

    refactor: removed unused selector

--- slices/shared/lib/store/features/board/selectors/current/values/index.ts
+++ slices/shared/lib/store/features/board/selectors/current/values/index.ts
@@ -1,5 +1,4 @@
 export * from "./selectCurrentStatValue";
-export * from "./selectCurrentStatValues";
 export * from "./selectCurrentStatBaseValue";
 export * from "./selectCurrentStatInitialValue";
 export * from "./stat";
