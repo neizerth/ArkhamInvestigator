79fff9f090bd4362b1f9c09eefef7a8d84d8f4cd
Vladimir Yazykov <neizerth@gmail.com>
2025-06-13 23:02:38 +0300

    refactor: create filter type

--- slices/modules/board/history/shared/lib/createHistoryActionFilter.ts
+++ slices/modules/board/history/shared/lib/createHistoryActionFilter.ts
@@ -1,16 +1,9 @@
 import type { ChangeBoardEventPayload as Payload } from "@modules/board/base/shared/model";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
-type MatchCallback<
-	P extends Payload,
-	T extends string,
-	M = never,
-	E = never,
-> = (action: unknown) => action is PayloadAction<P, T, M, E>;
-
 export const createHistoryActionFilter =
 	<P extends Payload, T extends string, M = never, E = never>(
-		match: MatchCallback<P, T, M, E>,
+		match: (action: unknown) => action is PayloadAction<P, T, M, E>,
 	) =>
 	(action: unknown): action is PayloadAction<P, T, M, E> => {
 		if (!match(action)) {
