65632a281bc6a71081633d9a7e0bdbce77323ce9
Vladimir Yazykov <neizerth@gmail.com>
2025-06-17 16:26:01 +0300

    refactor: simplify board thunk

--- slices/modules/board/base/shared/lib/store/thunks/value/setBoardPropValue.ts
+++ slices/modules/board/base/shared/lib/store/thunks/value/setBoardPropValue.ts
@@ -1,31 +1,13 @@
-import type { AppThunk } from "@shared/model";
 import type { InvestigatorBoardStat as Key } from "../../../../model/board";
 import {
 	type ChangeBoardPropValuePayload,
 	changeBoardPropValue,
 } from "../../actions";
-import { selectBoardCode } from "../../selectors/props/static/selectBoardCode";
+import { createBoardThunk } from "../../util";
 
 export type SetBoardPropValuePayload<K extends Key> = Omit<
 	ChangeBoardPropValuePayload<K>,
 	"code"
 >;
 
-export const setBoardPropValue =
-	<K extends Key>(payload: SetBoardPropValuePayload<K>): AppThunk =>
-	(dispatch, getState) => {
-		const { boardId } = payload;
-		const state = getState();
-		const code = selectBoardCode(boardId)(state);
-
-		if (!code) {
-			return;
-		}
-
-		dispatch(
-			changeBoardPropValue({
-				...payload,
-				code,
-			}),
-		);
-	};
+export const setBoardPropValue = createBoardThunk(changeBoardPropValue);
