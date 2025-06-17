bb95ff164eee30308693ae2c5fc30de3a07ac238
Vladimir Yazykov <neizerth@gmail.com>
2025-06-17 16:13:32 +0300

    refactor: create board thunk hof

--- slices/modules/board/base/shared/lib/store/thunks/setBoardPart.ts
+++ slices/modules/board/base/shared/lib/store/thunks/setBoardPart.ts
@@ -1,24 +1,6 @@
-import { selectBoardCode } from "@modules/board/base/shared/lib";
-import type { AppThunk } from "@shared/model";
 import { type ChangeBoardPartPayload, changeBoardPart } from "../actions";
+import { createBoardThunk } from "../util";
 
 export type SetBoardPartPayload = Omit<ChangeBoardPartPayload, "code">;
 
-export const setBoardPart =
-	(options: SetBoardPartPayload): AppThunk =>
-	(dispatch, getState) => {
-		const { boardId } = options;
-		const state = getState();
-		const code = selectBoardCode(boardId)(state);
-
-		if (!code) {
-			return;
-		}
-
-		dispatch(
-			changeBoardPart({
-				...options,
-				code,
-			}),
-		);
-	};
+export const setBoardPart = createBoardThunk(changeBoardPart);
--- slices/modules/board/base/shared/lib/store/thunks/setBoardProp.ts
+++ slices/modules/board/base/shared/lib/store/thunks/setBoardProp.ts
@@ -1,28 +1,10 @@
-import { selectBoardCode } from "@modules/board/base/shared/lib";
 import type { BoardKey } from "@modules/board/base/shared/model";
-import type { AppThunk } from "@shared/model";
 import { type ChangeBoardPropPayload, changeBoardProp } from "../actions";
+import { createBoardThunk } from "../util";
 
 export type SetBoardPropPayload<K extends BoardKey> = Omit<
 	ChangeBoardPropPayload<K>,
 	"code"
 >;
 
-export const setBoardProp =
-	<K extends BoardKey>(options: SetBoardPropPayload<K>): AppThunk =>
-	(dispatch, getState) => {
-		const { boardId } = options;
-		const state = getState();
-		const code = selectBoardCode(boardId)(state);
-
-		if (!code) {
-			return;
-		}
-
-		dispatch(
-			changeBoardProp({
-				...options,
-				code,
-			}),
-		);
-	};
+export const setBoardProp = createBoardThunk(changeBoardProp);
