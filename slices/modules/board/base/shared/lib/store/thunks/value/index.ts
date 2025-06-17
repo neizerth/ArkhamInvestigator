97b9cfd9fbbc17879508e4c30782dfa76088b37e
Vladimir Yazykov <neizerth@gmail.com>
2025-06-17 17:18:56 +0300

    refactor: board value thunk creator

--- slices/modules/board/base/shared/lib/store/thunks/value/index.ts
+++ slices/modules/board/base/shared/lib/store/thunks/value/index.ts
@@ -2,3 +2,4 @@ export * from "./decreaseBoardValuePropInternal";
 export * from "./increaseBoardValuePropInternal";
 export * from "./reduceBoardValuePropInternal";
 export * from "./setBoardPropValue";
+export * from "./propValues";
