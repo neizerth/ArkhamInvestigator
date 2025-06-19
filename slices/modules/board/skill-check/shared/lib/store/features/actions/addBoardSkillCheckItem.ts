c0b2956843bcdea298caf332a45b689f0f81c7c3
Vladimir Yazykov <neizerth@gmail.com>
2025-06-19 22:54:13 +0300

    refactor: skill check actions

--- slices/modules/board/skill-check/shared/lib/store/features/actions/addBoardSkillCheckItem.ts
+++ slices/modules/board/skill-check/shared/lib/store/features/actions/addBoardSkillCheckItem.ts
@@ -3,10 +3,9 @@ import { skillCheckPrefix } from "@modules/board/skill-check/shared/config";
 import type { SkillCheckHistoryItem } from "@modules/board/skill-check/shared/model";
 import { createAction } from "@reduxjs/toolkit";
 
-export type AddBoardSkillCheckItem = PropsWithBoard & {
+export type AddBoardSkillCheckItemPayload = PropsWithBoard & {
 	value: SkillCheckHistoryItem;
 };
 
-export const addBoardSkillCheckItemAction = createAction(
-	`${skillCheckPrefix}/addItem`,
-);
+export const addBoardSkillCheckItemAction =
+	createAction<AddBoardSkillCheckItemPayload>(`${skillCheckPrefix}/addItem`);
