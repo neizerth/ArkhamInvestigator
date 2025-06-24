74b80980443f37ffb3b1f87d46da1ba6ff58cdd6
Vladimir Yazykov <neizerth@gmail.com>
2025-06-24 21:33:58 +0300

    refactor: move investigator abilities into group

---
+++ slices/modules/mechanics/board/abilities/entities/lib/store/sagas/investigators/index.ts
@@ -0,0 +1,14 @@
+import { spawn } from "redux-saga/effects";
+import { CalwinWrightAbilitySaga } from "./CalwinWrightAbilitySaga";
+import { DianaStanleyAbilitySaga } from "./DianaStanleyAbilitySaga";
+import { GeorgeBarnabyAbilitySaga } from "./GeorgeBarnabyAbilitySaga";
+import { ShatteredSelfAbilitySaga } from "./ShatteredSelfAbilitySaga";
+import { Subject5U21AbilitySaga } from "./Subject5U21AbilitySaga";
+
+export function* investigatorAbilitySaga() {
+	yield spawn(DianaStanleyAbilitySaga);
+	yield spawn(GeorgeBarnabyAbilitySaga);
+	yield spawn(Subject5U21AbilitySaga);
+	yield spawn(CalwinWrightAbilitySaga);
+	yield spawn(ShatteredSelfAbilitySaga);
+}
