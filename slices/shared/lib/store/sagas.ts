b0120be5aac077e9c958d60b186e8a215883ebf0
Vladimir Yazykov <neizerth@gmail.com>
2025-06-30 17:35:43 +0300

    refactor: moved abilities to features

--- slices/shared/lib/store/sagas.ts
+++ slices/shared/lib/store/sagas.ts
@@ -2,7 +2,7 @@ import { abilitiesSaga } from "@modules/board/abilities/shared/lib";
 import { boardSharedSaga } from "@modules/board/base/shared/lib";
 import { boardHistorySaga } from "@modules/board/history/shared/lib";
 import { skillCheckSaga } from "@modules/board/skill-check/shared/lib";
-import { boardEntityAbilityMechanicsSaga } from "@modules/mechanics/board/abilities/entities/lib/store/sagas";
+import { boardEntityAbilityMechanicsSaga } from "@modules/mechanics/board/abilities/features/lib/store/sagas";
 import { boardEntityMechanicsSaga } from "@modules/mechanics/board/base/entities/lib";
 import { spawn } from "redux-saga/effects";
 
