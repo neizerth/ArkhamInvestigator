7f9eec55dbf427defa4b27fefa7364d7b618cdd6
Vladimir Yazykov <neizerth@gmail.com>
2025-07-08 22:43:33 +0300

    fix: shattered self counter

--- slices/modules/mechanics/board/abilities/features/lib/store/sagas/investigators/ShatteredSelfAbilitySaga.ts
+++ slices/modules/mechanics/board/abilities/features/lib/store/sagas/investigators/ShatteredSelfAbilitySaga.ts
@@ -1,55 +1,35 @@
-import { setBoardValuePart } from "@modules/board/base/shared/lib";
-import { boardHistoryItemAdded } from "@modules/board/history/shared/lib";
-import { InvesigatorCode } from "@modules/mechanics/investigator/entities/config";
-import { put, takeEvery } from "redux-saga/effects";
+import { createAbilityValueFilter } from "@modules/board/abilities/shared/lib";
+import type { boardAbilityValueSet } from "@modules/board/abilities/shared/lib/store/actions";
+import {
+	selectBoardById,
+	setBoardValuePart,
+} from "@modules/board/base/shared/lib";
+import { put, select, takeEvery } from "redux-saga/effects";
 
-const filterAction = (action: unknown) => {
-	if (!boardHistoryItemAdded.match(action)) {
-		return false;
-	}
-	const { code, item } = action.payload;
+const filterAction = createAbilityValueFilter("shattered-self-cards");
 
-	if (code !== InvesigatorCode.ShatteredSelf) {
-		return false;
-	}
+function* worker({ payload }: ReturnType<typeof boardAbilityValueSet>) {
+	const { boardId, value, prevValue, historyItem } = payload;
 
-	return typeof item.value?.handSize === "number";
-};
+	const selectBoard = selectBoardById(boardId);
 
-function* worker({ payload }: ReturnType<typeof boardHistoryItemAdded>) {
-	const { boardId, board, item } = payload;
+	const board: ReturnType<typeof selectBoard> = yield select(selectBoard);
 
-	const handSize = item.value?.handSize;
-
-	if (typeof handSize !== "number") {
-		return;
-	}
-
-	const prevValue = board.value.handSize;
-
-	const diff = Math.min(5, handSize) - Math.min(5, prevValue);
-
-	if (diff === 0) {
-		return;
-	}
-
-	const { willpower, intellect, combat, agility } = board.value;
-
-	const change = {
-		willpower: willpower + diff,
-		intellect: intellect + diff,
-		combat: combat + diff,
-		agility: agility + diff,
-	};
+	const diff = value - prevValue;
 
 	yield put(
 		setBoardValuePart({
 			boardId,
 			type: "value",
-			value: change,
+			value: {
+				willpower: board.value.willpower + diff,
+				intellect: board.value.intellect + diff,
+				combat: board.value.combat + diff,
+				agility: board.value.agility + diff,
+			},
 			history: {
 				type: "update",
-				id: item.id,
+				id: historyItem.id,
 			},
 		}),
 	);
--- slices/modules/mechanics/board/abilities/features/lib/store/sagas/investigators/index.ts
+++ slices/modules/mechanics/board/abilities/features/lib/store/sagas/investigators/index.ts
@@ -1,12 +1,15 @@
 import { spawn } from "redux-saga/effects";
 import { CalwinWrightAbilitySaga } from "./CalwinWrightAbilitySaga";
 import { DianaStanleyAbilitySaga } from "./DianaStanleyAbilitySaga";
+import { GeorgeBarnabyAbilitySaga } from "./GeorgeBarnabyAbilitySaga";
+import { ShatteredSelfAbilitySaga } from "./ShatteredSelfAbilitySaga";
+import { Subject5U21AbilitySaga } from "./Subject5U21AbilitySaga";
 
 export function* investigatorAbilitiesSaga() {
 	yield spawn(DianaStanleyAbilitySaga);
-	// yield spawn(GeorgeBarnabyAbilitySaga);
-	// yield spawn(Subject5U21AbilitySaga);
+	yield spawn(GeorgeBarnabyAbilitySaga);
+	yield spawn(Subject5U21AbilitySaga);
 	yield spawn(CalwinWrightAbilitySaga);
-	// yield spawn(ShatteredSelfAbilitySaga);
+	yield spawn(ShatteredSelfAbilitySaga);
 	// yield spawn(ZoeySamarasAbilitySaga);
 }
