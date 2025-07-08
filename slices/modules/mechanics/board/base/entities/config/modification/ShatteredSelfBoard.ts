cebf11a28d0a29232db9db893fe66f6544b3267b
Vladimir Yazykov <neizerth@gmail.com>
2025-07-08 22:50:56 +0300

    feat: shattered self initial board

--- slices/modules/mechanics/board/base/entities/config/modification/ShatteredSelfBoard.ts
+++ slices/modules/mechanics/board/base/entities/config/modification/ShatteredSelfBoard.ts
@@ -3,14 +3,26 @@ import type { InvestigatorBoardModification } from "../../model";
 import { DEFAULT_HAND_SIZE } from "../stat";
 
 const handSize = DEFAULT_HAND_SIZE - 3;
+const abilityId = "shattered-self-cards";
 
 export const ShatteredSelfBoard: InvestigatorBoardModification = {
-	[InvesigatorCode.ShatteredSelf]: () => ({
-		baseValue: {
-			handSize,
-		},
-		value: {
-			handSize,
-		},
-	}),
+	[InvesigatorCode.ShatteredSelf]: () => {
+		const value = 5;
+
+		return {
+			baseValue: {
+				handSize,
+			},
+			value: {
+				handSize,
+				willpower: value,
+				intellect: value,
+				combat: value,
+				agility: value,
+			},
+			abilityValues: {
+				[abilityId]: value,
+			},
+		};
+	},
 };
