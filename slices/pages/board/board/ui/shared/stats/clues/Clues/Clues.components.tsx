35bef9119583cbdb9654b87ea4a9aedb73425a9c
Vladimir Yazykov <neizerth@gmail.com>
2025-07-08 20:12:47 +0300

    feat: moved clues sync to main board

--- slices/pages/board/board/ui/shared/stats/clues/Clues/Clues.components.tsx
+++ slices/pages/board/board/ui/shared/stats/clues/Clues/Clues.components.tsx
@@ -1,6 +1,11 @@
+import {
+	IconButton,
+	type IconButtonProps,
+} from "@modules/core/haptic/shared/ui";
 import { color, gameAssets } from "@shared/config";
 import * as UI from "@shared/ui";
 import { Value as BaseValue } from "@shared/ui";
+import type { FC } from "react";
 import styled from "styled-components/native";
 import { assetsSize } from "../../../../../config";
 import { withStat } from "../../../../../lib";
@@ -26,3 +31,24 @@ export const Picker: typeof StatPicker = styled(StatPicker).attrs({
 })`
   
   `;
+
+type LockProps = IconButtonProps & {
+	enabled?: boolean;
+};
+
+export const Lock: FC<LockProps> = styled(IconButton).attrs(
+	({ enabled }: LockProps) => ({
+		iconStyle: {
+			color: enabled ? "rgb(86, 122, 45)" : color.dark10,
+			fontSize: 24,
+		},
+	}),
+)`
+	color:rgb(86, 122, 45);
+  position: absolute;
+  z-index: 2;
+  right: -25px;
+  top: -57px;
+	width: 50px;
+	align-items: flex-start;
+`;
