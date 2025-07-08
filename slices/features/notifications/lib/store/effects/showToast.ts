27a14b72e7f5e21cc9f1405441a98e1094576a20
Vladimir Yazykov <neizerth@gmail.com>
2025-07-09 01:42:44 +0300

    feat: new toasts

--- slices/features/notifications/lib/store/effects/showToast.ts
+++
@@ -1,8 +0,0 @@
-import type { AppThunk } from "@shared/model";
-import Toast from "react-native-simple-toast";
-
-export const showToast =
-	(message: string, duration = Toast.SHORT): AppThunk =>
-	() => {
-		Toast.show(message, duration);
-	};
