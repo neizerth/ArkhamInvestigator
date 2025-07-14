e94ac500b0fcac79648c02bdf1285c88d7858f6c
Vladimir Yazykov <neizerth@gmail.com>
2025-07-14 12:28:49 +0300

    refactor: cant add token notification

--- slices/modules/chaos-bag/base/features/lib/store/sagas/notifications/cantAddChaosToken/cantAddMultipleChaosTokensSaga.ts
+++ slices/modules/chaos-bag/base/features/lib/store/sagas/notifications/cantAddChaosToken/cantAddMultipleChaosTokensSaga.ts
@@ -1,9 +1,12 @@
 import { cantAddMultipleChaosTokens } from "@modules/chaos-bag/base/entities/lib";
+import { chaosToken } from "@modules/chaos-bag/base/shared/config";
 import { sendNotification } from "@modules/core/notifications/shared/lib";
 import { put, takeEvery } from "redux-saga/effects";
 
 function* worker({ payload }: ReturnType<typeof cantAddMultipleChaosTokens>) {
-	const { type, count } = payload;
+	const { type, count, limit, available } = payload;
+
+	const character = chaosToken.character[type];
 
 	yield put(
 		sendNotification({
@@ -13,6 +16,9 @@ function* worker({ payload }: ReturnType<typeof cantAddMultipleChaosTokens>) {
 			data: {
 				type,
 				count,
+				character,
+				limit,
+				available,
 			},
 		}),
 	);
--- slices/modules/chaos-bag/base/features/lib/store/sagas/notifications/cantAddChaosToken/cantAddSingleChaosTokenSaga.ts
+++ slices/modules/chaos-bag/base/features/lib/store/sagas/notifications/cantAddChaosToken/cantAddSingleChaosTokenSaga.ts
@@ -1,9 +1,12 @@
 import { cantAddSingleChaosToken } from "@modules/chaos-bag/base/entities/lib";
+import { chaosToken } from "@modules/chaos-bag/base/shared/config";
 import { sendNotification } from "@modules/core/notifications/shared/lib";
 import { put, takeEvery } from "redux-saga/effects";
 
 function* worker({ payload }: ReturnType<typeof cantAddSingleChaosToken>) {
-	const { type } = payload;
+	const { type, limit, available } = payload;
+
+	const character = chaosToken.character[type];
 
 	yield put(
 		sendNotification({
@@ -12,6 +15,9 @@ function* worker({ payload }: ReturnType<typeof cantAddSingleChaosToken>) {
 			message: "chaosBag.cantAddToken",
 			data: {
 				type,
+				character,
+				limit,
+				available,
 				count: 1,
 			},
 		}),
