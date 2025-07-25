252bc071221f6ddfcb28985d4e7e341ef9d39201
Vladimir Yazykov <neizerth@gmail.com>
2025-07-25 14:01:00 +0300

    feat: notifications ui

--- slices/modules/core/notifications/shared/lib/toast/showToast.ts
+++ slices/modules/core/notifications/shared/lib/toast/showToast.ts
@@ -1,26 +1,23 @@
+import type { Faction } from "@shared/model";
+import { pick } from "ramda";
 import Toast, { type ToastShowParams } from "react-native-toast-message";
 
 export type ShowToastOptions = ToastShowParams & {
 	title?: string;
+	faction?: Faction;
+	faction2?: Faction;
 	image1?: string;
 	image2?: string;
 	message: string;
 };
 
-export const showToast = ({
-	message,
-	title,
-	image1,
-	image2,
-	...props
-}: ShowToastOptions) => {
+export const showToast = ({ message, title, ...props }: ShowToastOptions) => {
+	const toastProps = pick(["image1", "image2", "faction", "faction2"], props);
+
 	Toast.show({
 		...props,
 		text2: title,
 		text1: message,
-		props: {
-			image1,
-			image2,
-		},
+		props: toastProps,
 	});
 };
--- slices/modules/core/notifications/shared/ui/Toast/Toast.tsx
+++ slices/modules/core/notifications/shared/ui/Toast/Toast.tsx
@@ -1,3 +1,4 @@
+import type { Faction } from "@shared/model";
 import type { BaseToastProps, ToastType } from "react-native-toast-message";
 import * as C from "./Toast.components";
 
@@ -6,17 +7,25 @@ export type ToastProps = BaseToastProps & {
 	props: {
 		image1?: string;
 		image2?: string;
+		faction?: Faction;
+		faction2?: Faction;
 	};
 };
 
 export const Toast = ({ type, text1, ...restProps }: ToastProps) => {
-	const { image1, image2 } = restProps.props;
+	const { image1, image2, faction, faction2 } = restProps.props;
 	return (
 		<C.Container type={type}>
 			<C.Content>
-				{image1 && <C.Img source={{ uri: image1 }} />}
+				{image1 && (
+					<C.Images>
+						<C.SourceImage source={{ uri: image1 }} faction={faction} />
+						{image2 && (
+							<C.TargetImage source={{ uri: image2 }} faction={faction2} />
+						)}
+					</C.Images>
+				)}
 				<C.Body>{text1 && <C.Text1 value={text1} />}</C.Body>
-				{image2 && <C.Img source={{ uri: image2 }} />}
 			</C.Content>
 		</C.Container>
 	);
