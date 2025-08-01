904a22513aeaba7fa12d5a846bfbe721aa1c68d3
Vladimir Yazykov <neizerth@gmail.com>
2025-08-01 20:08:30 +0300

    feat: ui action filtering

--- slices/modules/core/ui/lib/store/util/createUIEventFilter.ts
+++ slices/modules/core/ui/lib/store/util/createUIEventFilter.ts
@@ -1,8 +1,30 @@
+import type { UIEventType } from "@modules/core/ui/model";
 import { createPayloadFilter } from "@shared/lib";
 import { UIEvent, type UIEventPayload } from "../actions";
 
-export type Options = Partial<UIEventPayload>;
+type Criteria = Partial<UIEventPayload> & {
+	types?: UIEventType[];
+};
 
-export const createUIEventFilter = createPayloadFilter({
+const filterAction = createPayloadFilter({
 	actionCreator: UIEvent,
 });
+
+export const createUIEventFilter = (criteria: Criteria) => {
+	const filter = filterAction(criteria);
+
+	return (action: unknown) => {
+		if (!UIEvent.match(action)) {
+			return false;
+		}
+
+		const { types } = criteria;
+		const { payload } = action;
+
+		if (types && !types.includes(payload.type)) {
+			return false;
+		}
+
+		return filter(action);
+	};
+};
