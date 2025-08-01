88dac00759c37497d0cc818f68f490819753d3d0
Vladimir Yazykov <neizerth@gmail.com>
2025-08-01 20:08:30 +0300

    feat: ui action filtering

--- slices/modules/core/haptic/features/trigger-haptic-on-ui-event/triggerHapticOnUIEventSaga.ts
+++ slices/modules/core/haptic/features/trigger-haptic-on-ui-event/triggerHapticOnUIEventSaga.ts
@@ -1,11 +1,16 @@
-import { UIEvent } from "@modules/core/ui/lib";
+import { createUIEventFilter } from "@modules/core/ui/lib/store/util";
 import { put, takeEvery } from "redux-saga/effects";
 import { triggerHapticFeedback } from "../../shared/lib";
+
+const filterAction = createUIEventFilter({
+	types: ["changing", "focus"],
+	canceled: false,
+});
 
 function* worker() {
 	yield put(triggerHapticFeedback({}));
 }
 
 export function* triggerHapticOnUIEventSaga() {
-	yield takeEvery(UIEvent.match, worker);
+	yield takeEvery(filterAction, worker);
 }
--- slices/shared/lib/util/store/createPayloadFilter.ts
+++ slices/shared/lib/util/store/createPayloadFilter.ts
@@ -1,12 +1,12 @@
 import type { ActionCreatorWithPayload } from "@reduxjs/toolkit";
 import { partiallyEquals } from "../../util/criteria";
 
-type Options<Payload> = {
+export type CreatePayloadFilterOptions<Payload> = {
 	actionCreator: ActionCreatorWithPayload<Payload>;
 };
 
 export const createPayloadFilter =
-	<Payload>({ actionCreator }: Options<Payload>) =>
+	<Payload>({ actionCreator }: CreatePayloadFilterOptions<Payload>) =>
 	(criteria: Partial<Payload>) => {
 		const equals = partiallyEquals(criteria);
 		return (action: unknown) => {
