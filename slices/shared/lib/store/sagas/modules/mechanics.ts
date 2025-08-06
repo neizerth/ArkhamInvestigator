e3a815907c5d40999b53497c129c23612c65c414
Vladimir Yazykov <neizerth@gmail.com>
2025-08-06 15:06:50 +0300

    refactor: renamed folder

--- slices/shared/lib/store/sagas/modules/mechanics.ts
+++ slices/shared/lib/store/sagas/modules/mechanics.ts
@@ -1,4 +1,4 @@
-import { boardEntityAbilityMechanicsSaga } from "@modules/mechanics/board/abilities/features/lib/store/sagas";
+import { boardEntityAbilityMechanicsSaga } from "@modules/mechanics/board/abilities/features/lib/store/features";
 import { boardEntityMechanicsSaga } from "@modules/mechanics/board/base/entities/lib/store/sagas";
 import { boardFeatureMechanicsSaga } from "@modules/mechanics/board/base/features/sagas";
 import { spawn } from "redux-saga/effects";
