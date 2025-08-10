45934a5a4b043d0fae42dc425efc59df81a333a3
Vladimir Yazykov <neizerth@gmail.com>
2025-08-10 15:16:18 +0300

    refactor: removed store icons

--- slices/shared/lib/store/features/icons/selectors/selectIcon.ts
+++
@@ -1,5 +0,0 @@
-import { createSelector } from "@reduxjs/toolkit";
-import { selectIcons } from "../icons";
-
-export const selectIcon = (code: string) =>
-	createSelector([selectIcons], (icons) => icons[code]);
--- slices/shared/lib/store/features/reducer.ts
+++ slices/shared/lib/store/features/reducer.ts
@@ -2,5 +2,4 @@ export { default as app } from "./app/app";
 export { default as game } from "./game/game";
 export { default as settings } from "./settings/settings";
 export { default as stories } from "./stories/stories";
-export { default as icons } from "./icons/icons";
 export { default as investigators } from "./investigators/investigators";
