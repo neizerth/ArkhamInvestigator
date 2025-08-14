74c4d7c69c6ca618b68be8d17c8f9bc798567f5e
Vladimir Yazykov <neizerth@gmail.com>
2025-08-14 22:14:37 +0300

    fix: circular deps

--- slices/modules/core/modal/entities/base/lib/store/sagas/customActions/changeInvestigatorDetailsModalActionSaga.ts
+++ slices/modules/core/modal/entities/base/lib/store/sagas/customActions/changeInvestigatorDetailsModalActionSaga.ts
@@ -1,10 +1,10 @@
 import { ModalActionId } from "@modules/core/modal/entities/base/config";
 import { createCustomModalActionSaga } from "@modules/core/modal/shared/base/lib";
-import { changeInvestigatorDetails } from "@modules/signature/shared/lib";
+import { changeSignatureDetails } from "@modules/signature/entities/changeSignatureDetails/changeSignatureDetails";
 import { put } from "redux-saga/effects";
 
 function* worker() {
-	yield put(changeInvestigatorDetails());
+	yield put(changeSignatureDetails());
 }
 
 export const changeInvestigatorDetailsModalActionSaga =
--- slices/modules/signature/entities/sagas.ts
+++ slices/modules/signature/entities/sagas.ts
@@ -1,6 +1,8 @@
 import { spawn } from "redux-saga/effects";
+import { changeSignatureDetailsSaga } from "./changeSignatureDetails";
 import { updateBoardSignaturesSaga } from "./updateBoardSignatures";
 
 export function* signatureEntitiesSaga() {
 	yield spawn(updateBoardSignaturesSaga);
+	yield spawn(changeSignatureDetailsSaga);
 }
