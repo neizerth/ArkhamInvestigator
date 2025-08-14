ca98052eda051d9cf2a0148ec947c557e1412c4d
Vladimir Yazykov <neizerth@gmail.com>
2025-08-14 16:41:52 +0300

    refactor: i18n

--- slices/modules/core/i18n/shared/lib/store/i18n.ts
+++ slices/modules/core/i18n/shared/lib/store/i18n.ts
@@ -1,18 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
-import type { ArkhamDBInvestigatorCard } from "@shared/model/api/game/arkhamDB";
 import { createSliceState } from "redux-toolkit-helpers";
 import { DEFAULT_LANGUAGE } from "../../config";
 
 export type I18NState = {
 	language: string | null;
 	availableLanguages: string[];
-	investigatorTranslations: ArkhamDBInvestigatorCard[];
 };
 
 const initialState: I18NState = {
 	language: null,
 	availableLanguages: [DEFAULT_LANGUAGE],
-	investigatorTranslations: [],
 };
 
 export const i18n = createSlice({
@@ -20,16 +17,8 @@ export const i18n = createSlice({
 	...createSliceState(initialState),
 });
 
-export const {
-	setLanguage,
-	setAvailableLanguages,
-	setInvestigatorTranslations,
-} = i18n.actions;
+export const { setLanguage, setAvailableLanguages } = i18n.actions;
 
-export const {
-	selectLanguage,
-	selectAvailableLanguages,
-	selectInvestigatorTranslations,
-} = i18n.selectors;
+export const { selectLanguage, selectAvailableLanguages } = i18n.selectors;
 
 export default i18n.reducer;
--- slices/modules/core/i18n/shared/lib/store/thunks/translations/index.ts
+++ slices/modules/core/i18n/shared/lib/store/thunks/translations/index.ts
@@ -1,3 +1,1 @@
-export * from "./fetchTranslationData";
-export * from "./loadContentTranslation";
 export * from "./loadContentTranslation";
