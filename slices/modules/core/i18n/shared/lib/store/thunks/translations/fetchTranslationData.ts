8d9400b98494a538e0e8d4c381f40faa08d44a1d
Vladimir Yazykov <neizerth@gmail.com>
2025-08-14 16:41:52 +0300

    refactor: i18n

--- slices/modules/core/i18n/shared/lib/store/thunks/translations/fetchTranslationData.ts
+++
@@ -1,7 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { loadGameTranslationData } from "@shared/api";
-
-export const fetchTranslationData = createAsyncThunk(
-	"i18n/loadTranslationsData",
-	loadGameTranslationData,
-);
