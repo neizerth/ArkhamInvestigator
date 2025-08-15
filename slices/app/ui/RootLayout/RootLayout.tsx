2fb8b88782e9e6d780bd5fe55f08fb718e0e0d1d
Vladimir Yazykov <neizerth@gmail.com>
2025-08-15 14:36:52 +0300

    fix: modal layout

--- slices/app/ui/RootLayout/RootLayout.tsx
+++ slices/app/ui/RootLayout/RootLayout.tsx
@@ -1,4 +1,5 @@
 import { AppLayout } from "@modules/core/app/app/ui";
+import { asTransparentModal } from "@modules/core/router/shared/lib/config";
 import { Stack } from "expo-router";
 import type { PropsWithChildren } from "react";
 import { screenOptions } from "./RootLayout.config";
@@ -6,7 +7,9 @@ import { screenOptions } from "./RootLayout.config";
 export const RootLayout = ({ children }: PropsWithChildren) => {
 	return (
 		<AppLayout>
-			<Stack screenOptions={screenOptions} />
+			<Stack screenOptions={screenOptions}>
+				<Stack.Screen name="(modal)" options={asTransparentModal} />
+			</Stack>
 
 			{children}
 		</AppLayout>
--- slices/shared/config/routes.ts
+++ slices/shared/config/routes.ts
@@ -8,21 +8,21 @@ export const routes = {
 	settings: r("/settings"),
 
 	selectInvestigators: r("/select-investigators"),
-	selectInvestigatorDetails: r("/modal/select-investigator-details"),
+	selectInvestigatorDetails: r("/(modal)/select-investigator-details"),
 
 	board: r("/board"),
 	boardHelp: r("/board/help"),
 	skillCheck: r("/board/skill-check"),
 	replaceInvestigator: r("/board/replace-investigator"),
-	overview: r("/modal/board/overview"),
-	roundReference: r("/modal/board/round-reference"),
+	overview: r("/(modal)/board/overview"),
+	roundReference: r("/(modal)/board/round-reference"),
 
 	chaosBag: r("/chaos-bag"),
-	chaosBagPreview: r("/modal/chaos-bag/preview"),
+	chaosBagPreview: r("/(modal)/chaos-bag/preview"),
 	chaosBagHistory: r("/chaos-bag/history"),
-	chaosBagFill: r("/modal/chaos-bag/fill"),
-	chaosBagReferenceEdit: r("/modal/chaos-bag/reference/edit"),
-	chaosBagReferenceView: r("/modal/chaos-bag/reference"),
+	chaosBagFill: r("/(modal)/chaos-bag/fill"),
+	chaosBagReferenceEdit: r("/(modal)/chaos-bag/reference/edit"),
+	chaosBagReferenceView: r("/(modal)/chaos-bag/reference"),
 
 	about: r("/static/about"),
 	support: r("/static/support"),
