fedf16d8e7710e50dacd28878070cd3c0bc1921b
Vladimir Yazykov <neizerth@gmail.com>
2025-08-15 08:31:56 +0300

    refactor: modal presentation

--- slices/modules/core/router/shared/lib/config/stack.ts
+++ slices/modules/core/router/shared/lib/config/stack.ts
@@ -2,22 +2,20 @@ import type { Stack } from "expo-router";
 import type { FC } from "react";
 
 type StackProps = typeof Stack extends FC<infer Props> ? Props : never;
+type ScreenOptions = StackProps["screenOptions"];
 
-export const stackScreenOptions: StackProps["screenOptions"] = {
+export const stackScreenOptions: ScreenOptions = {
 	headerShown: false,
 	contentStyle: {
 		backgroundColor: "transparent",
 	},
-};
-
-export const modalStackScreenOptions: StackProps["screenOptions"] = {
-	headerShown: false,
-	contentStyle: {
-		backgroundColor: "transparent",
-	},
-	presentation: "transparentModal",
 };
 
 export const asTransparentModal = {
 	presentation: "transparentModal" as const,
+};
+
+export const modalStackScreenOptions: ScreenOptions = {
+	...stackScreenOptions,
+	...asTransparentModal,
 };
