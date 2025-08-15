f0a17cb0cf7d96b9dc16b627afdeb9c4b8d5b20b
Vladimir Yazykov <neizerth@gmail.com>
2025-08-15 14:36:52 +0300

    fix: modal layout

--- app/modal/_layout.tsx
+++
@@ -1,6 +0,0 @@
-import { modalStackScreenOptions } from "@modules/core/router/shared/lib/config";
-import { Stack } from "expo-router";
-
-export default function ModalLayout() {
-	return <Stack screenOptions={modalStackScreenOptions} />;
-}
