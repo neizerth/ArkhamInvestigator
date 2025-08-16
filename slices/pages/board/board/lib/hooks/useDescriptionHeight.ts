5478bc7b60653b8106af096700d9a935f23c7cfb
Vladimir Yazykov <neizerth@gmail.com>
2025-08-16 22:44:38 +0300

    fix: edge to edge navbar

--- slices/pages/board/board/lib/hooks/useDescriptionHeight.ts
+++ slices/pages/board/board/lib/hooks/useDescriptionHeight.ts
@@ -1,4 +1,4 @@
-import { CAN_ALWAYS_SHOW_GAME_TEXT, navBarHeight } from "@shared/config";
+import { CAN_ALWAYS_SHOW_GAME_TEXT } from "@shared/config";
 
 import { useAppSelector } from "@shared/lib";
 
@@ -8,8 +8,6 @@ import {
 } from "@modules/board/base/shared/lib";
 import { DEFAULT_PORTRAIT_DESCRIPTION_HEIGHT } from "../../config";
 
-const isNavbarVisible = navBarHeight > 0;
-
 export const useDescriptionHeight = () => {
 	const show = useAppSelector(selectAlwaysShowGameText);
 	const gameTextHeight = useAppSelector(selectGameTextHeight);
@@ -18,7 +16,7 @@ export const useDescriptionHeight = () => {
 		return DEFAULT_PORTRAIT_DESCRIPTION_HEIGHT;
 	}
 
-	const gap = isNavbarVisible ? navBarHeight - 5 : 50;
+	const gap = 50;
 
 	const fullHeight = gameTextHeight + gap;
 
