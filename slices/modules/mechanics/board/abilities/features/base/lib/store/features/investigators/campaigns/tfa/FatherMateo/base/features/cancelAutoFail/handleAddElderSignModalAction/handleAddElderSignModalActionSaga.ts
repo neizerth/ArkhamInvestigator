0956e8f874fa7066109aab7969abdea40df19056
Vladimir Yazykov <neizerth@gmail.com>
2025-08-17 10:59:19 +0300

    fix: mateo ability

--- slices/modules/mechanics/board/abilities/features/base/lib/store/features/investigators/campaigns/tfa/FatherMateo/base/features/cancelAutoFail/handleAddElderSignModalAction/handleAddElderSignModalActionSaga.ts
+++ slices/modules/mechanics/board/abilities/features/base/lib/store/features/investigators/campaigns/tfa/FatherMateo/base/features/cancelAutoFail/handleAddElderSignModalAction/handleAddElderSignModalActionSaga.ts
@@ -3,11 +3,13 @@ import { addRevealedTokens } from "@modules/chaos-bag/reveal/base/shared/lib";
 import type { RevealedChaosBagToken } from "@modules/chaos-bag/reveal/base/shared/model";
 import { openChaosTokenRevealModal } from "@modules/chaos-bag/reveal/modal/entities/lib";
 import { selectChaosBagTokenValues } from "@modules/chaos-bag/value/entities/lib";
+import type { ConfirmModalAction } from "@modules/core/modal/shared/actions/confirm/model";
 import {
+	type ModalActionProcessedPayload,
 	createModalActionFilter,
-	type modalActionProcessed,
 } from "@modules/core/modal/shared/base/lib";
 import { AbilityCode } from "@modules/mechanics/board/abilities/shared/config";
+import type { PayloadAction } from "@reduxjs/toolkit";
 import { put, select, takeEvery } from "redux-saga/effects";
 import { v4 } from "uuid";
 import { addElderSignModalActionId, elderSignTokenId } from "../../../config";
@@ -18,8 +20,16 @@ const filterAction = createModalActionFilter({
 
 const abilityId = AbilityCode.FatherMateo.base;
 
-function* worker({ payload }: ReturnType<typeof modalActionProcessed>) {
-	const { boardId } = payload;
+type ModalAction = ConfirmModalAction<{
+	sourceBoardId: number;
+}>;
+
+type Payload = ModalActionProcessedPayload<ModalAction>;
+type Action = PayloadAction<Payload>;
+
+function* worker({ payload }: Action) {
+	const { boardId, modalAction } = payload;
+	const { sourceBoardId } = modalAction.data;
 	const valuesSelector = selectChaosBagTokenValues(boardId);
 	const values: ReturnType<typeof valuesSelector> =
 		yield select(valuesSelector);
@@ -44,7 +54,7 @@ function* worker({ payload }: ReturnType<typeof modalActionProcessed>) {
 
 	yield put(
 		setBoardAbilityUse({
-			boardId,
+			boardId: sourceBoardId,
 			abilityId,
 			canUse: false,
 		}),
