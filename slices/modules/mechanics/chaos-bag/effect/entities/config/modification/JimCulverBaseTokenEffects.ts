f0b52c463128327c8318a2f94ebbe4b1a5117ae9
Vladimir Yazykov <neizerth@gmail.com>
2025-08-20 00:26:54 +0300

    fix: jim text

--- slices/modules/mechanics/chaos-bag/effect/entities/config/modification/JimCulverBaseTokenEffects.ts
+++ slices/modules/mechanics/chaos-bag/effect/entities/config/modification/JimCulverBaseTokenEffects.ts
@@ -6,7 +6,7 @@ import type { InvestigatorTokenEffectModification as Modification } from "../../
 
 export const JimCulverBaseTokenEffects: Modification = {
 	[InvesigatorCode.JimCulver.base]: (options) => {
-		const { referenceCardEffects, tokenValues } = options;
+		const { referenceCardEffects, tokenValues, board } = options;
 		const effects = defultEffectModificationCallback(options);
 
 		const skullValue = tokenValues.skull;
@@ -19,10 +19,17 @@ export const JimCulverBaseTokenEffects: Modification = {
 			value: skullValue,
 		});
 
+		const lines = board.investigator.text.split("\n");
+		const line = lines[1];
+
+		const elderSignEffects = effects.elderSign
+			? `${effects.elderSign}\n${line}`
+			: line;
+
 		return {
 			...effects,
 			skull: `${effects.skull}\n<i>${skullEffect}</i>`,
-			elderSign: `${effects.elderSign}\n<i>${referenceCardEffects.skull}</i>`,
+			elderSign: `${elderSignEffects}\n<i>${referenceCardEffects.skull}</i>`,
 		};
 	},
 };
