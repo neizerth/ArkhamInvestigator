e59d2425ae8ea95ae2829027fc2e776f241264ea
Vladimir Yazykov <neizerth@gmail.com>
2025-08-19 23:10:01 +0300

    fix: token list

--- slices/modules/chaos-bag/base/entities/ui/control/ChaosTokenList/data.ts
+++ slices/modules/chaos-bag/base/entities/ui/control/ChaosTokenList/data.ts
@@ -1,11 +1,14 @@
 import { chaosToken } from "@modules/chaos-bag/base/shared/config";
+import type { ChaosTokenType } from "@modules/chaos-bag/base/shared/model";
 import type { ChaosTokenListItem } from "./ChaosTokenList.types";
+
+const data: ChaosTokenType[] = [...chaosToken.types.base, "frost"];
 
 export const chaosTokenListData: ChaosTokenListItem[] = [
 	{
 		type: "blessCurse",
 	},
-	...chaosToken.types.base.map(
+	...data.map(
 		(value): ChaosTokenListItem => ({
 			type: "token",
 			value,
