1d22302acda16a00591b2ca0b6e171c2a25290fd
Vladimir Yazykov <neizerth@gmail.com>
2025-08-19 23:10:29 +0300

    fix: navbar padding

--- slices/widgets/content/content-page/ui/ContentPage/ContentPage.components.tsx
+++ slices/widgets/content/content-page/ui/ContentPage/ContentPage.components.tsx
@@ -1,4 +1,4 @@
-import { size } from "@shared/config";
+import { navBarHeight, size } from "@shared/config";
 import { View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import styled from "styled-components/native";
@@ -9,7 +9,7 @@ export const FullContent: typeof View = styled(View)`
 
 export const Content: typeof ScrollView = styled(ScrollView)`
   flex: 1;
-  padding: ${size.gap.medium}px;
+  padding: ${size.gap.medium}px ${size.gap.medium}px ${navBarHeight}px;
   padding-top: 0;
   margin-bottom: ${size.gap.default}px;
 `;
