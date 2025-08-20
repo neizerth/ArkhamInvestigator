fc43751ee3405b7798a2228c1a57a8399eba1e13
Vladimir Yazykov <neizerth@gmail.com>
2025-08-20 23:16:02 +0300

    refactor: rename saga file

--- slices/modules/mechanics/board/abilities/features/base/lib/store/features/investigators/sagas.ts
+++ slices/modules/mechanics/board/abilities/features/base/lib/store/features/investigators/sagas.ts
@@ -1,6 +1,6 @@
 import { fork } from "redux-saga/effects";
 import { campaignsInvestigatorAbilitySaga as campaignsSaga } from "./campaigns/sagas";
-import { sideScenariosInvestigatorAbilitySaga as sideScenariosSaga } from "./side";
+import { sideScenariosInvestigatorAbilitySaga as sideScenariosSaga } from "./side/sagas";
 
 export function* investigatorAbilitiesSaga() {
 	yield fork(campaignsSaga);
