aabf4e322c542916d217e3d04e9248ce4817dd00
Vladimir Yazykov <neizerth@gmail.com>
2025-08-21 17:39:31 +0300

    fix: stella elder sign

--- slices/modules/mechanics/board/abilities/features/base/lib/store/features/investigators/decks/StellaClark/elderSign/openElderSignModalSaga.ts
+++ slices/modules/mechanics/board/abilities/features/base/lib/store/features/investigators/decks/StellaClark/elderSign/openElderSignModalSaga.ts
@@ -7,10 +7,7 @@ import {
 import { createCancelModalAction } from "@modules/core/modal/shared/actions/cancel/lib";
 import { createConfirmModalAction } from "@modules/core/modal/shared/actions/confirm/lib";
 import { openConfirm } from "@modules/core/modal/shared/confirm/lib";
-import {
-	getBoardDamage,
-	getBoardHorror,
-} from "@modules/mechanics/board/base/entities/lib";
+
 import { InvesigatorCode } from "@modules/mechanics/investigator/entities/config";
 import { put, select, takeEvery } from "redux-saga/effects";
 import { modalActionId, modalId } from "../config";
@@ -24,13 +21,6 @@ function* worker({ payload }: ReturnType<typeof chaosTokensRevealed>) {
 	const { boardId } = payload;
 	const boardSelector = selectBoardById(boardId);
 	const board: ReturnType<typeof boardSelector> = yield select(boardSelector);
-
-	const damage = getBoardDamage(board);
-	const horror = getBoardHorror(board);
-
-	if (damage === 0 && horror === 0) {
-		return;
-	}
 
 	yield put(
 		openConfirm({
