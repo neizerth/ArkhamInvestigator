bc0a98544ba7374c46ccd5c562b8f0ce75bed928
Vladimir Yazykov <neizerth@gmail.com>
2025-08-21 11:01:38 +0300

    feat: version auto increment

---
+++ cli/increment-version.ts
@@ -0,0 +1,11 @@
+import { readFileSync, writeFileSync } from "node:fs";
+import path from "node:path";
+
+const file = path.join(__dirname, "../app.version.json");
+const data = JSON.parse(readFileSync(file, "utf8"));
+
+data.versionCode++;
+
+writeFileSync(file, JSON.stringify(data, null, 2));
+
+console.log("new app version:", data.version);
