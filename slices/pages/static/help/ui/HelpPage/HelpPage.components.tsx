611f935994555a124111c91ce1fdbb1b733cd1bb
Vladimir Yazykov <neizerth@gmail.com>
2025-08-24 07:58:31 +0300

    fix: help investigators text

--- slices/pages/static/help/ui/HelpPage/HelpPage.components.tsx
+++ slices/pages/static/help/ui/HelpPage/HelpPage.components.tsx
@@ -1,5 +1,5 @@
 import { color, font, size } from "@shared/config";
-import { GameText } from "@shared/ui";
+import { GameText, Row } from "@shared/ui";
 import { Icon as BaseIcon } from "@shared/ui";
 import { View } from "react-native";
 import styled from "styled-components/native";
@@ -18,4 +18,9 @@ export const ListItem: typeof View = styled(View)`
 export const IconText: typeof GameText = styled(GameText)`
   color: ${color.light10};
   font-size: ${font.size.default}px;
+`;
+
+export const ListTitle: typeof Row = styled(Row)`
+  align-items: center;
+  gap: ${size.gap.small}px
 `;
