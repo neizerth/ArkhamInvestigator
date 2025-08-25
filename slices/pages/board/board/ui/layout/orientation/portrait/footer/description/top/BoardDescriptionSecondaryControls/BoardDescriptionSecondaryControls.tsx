ec381da2b1d9342da0a30840e9dd63c4a18a107e
Vladimir Yazykov <neizerth@gmail.com>
2025-08-25 08:15:20 +0300

    feat: hide description on free area press

--- slices/pages/board/board/ui/layout/orientation/portrait/footer/description/top/BoardDescriptionSecondaryControls/BoardDescriptionSecondaryControls.tsx
+++ slices/pages/board/board/ui/layout/orientation/portrait/footer/description/top/BoardDescriptionSecondaryControls/BoardDescriptionSecondaryControls.tsx
@@ -2,8 +2,11 @@ import {
 	selectShowDoom,
 	selectShowScenarioClues,
 	selectShowScenarioResources,
+	setShowDescription,
 } from "@modules/board/base/shared/lib";
-import { useAppSelector } from "@shared/lib";
+import { useAppDispatch, useAppSelector } from "@shared/lib";
+import { Outside } from "@shared/ui";
+import { useCallback } from "react";
 import type { ViewProps } from "react-native";
 import {
 	ScenarioClues,
@@ -17,11 +20,16 @@ export type BoardDescriptionSecondaryControlsProps = ViewProps;
 export const BoardDescriptionSecondaryControls = (
 	props: BoardDescriptionSecondaryControlsProps,
 ) => {
+	const dispatch = useAppDispatch();
 	const showDoom = useAppSelector(selectShowDoom);
 	const showResources = useAppSelector(selectShowScenarioResources);
 	const showClues = useAppSelector(selectShowScenarioClues);
 
 	const noContent = !(showDoom || showResources || showClues);
+
+	const close = useCallback(() => {
+		dispatch(setShowDescription(false));
+	}, [dispatch]);
 
 	if (noContent) {
 		return null;
@@ -29,6 +37,7 @@ export const BoardDescriptionSecondaryControls = (
 
 	return (
 		<C.Container {...props}>
+			<Outside onPress={close} />
 			<C.Content>
 				{showDoom && <ScenarioDoom visible />}
 				{showResources && <ScenarioResources visible />}
