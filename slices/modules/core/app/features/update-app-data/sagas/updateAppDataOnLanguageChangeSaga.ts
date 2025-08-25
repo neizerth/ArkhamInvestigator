d3e19b6172049638244951aaeb6bff78c988ca6a
Vladimir Yazykov <neizerth@gmail.com>
2025-08-25 08:05:45 +0300

    fix: default language detection

--- slices/modules/core/app/features/update-app-data/sagas/updateAppDataOnLanguageChangeSaga.ts
+++ slices/modules/core/app/features/update-app-data/sagas/updateAppDataOnLanguageChangeSaga.ts
@@ -1,10 +1,11 @@
 import { updateAppData } from "@modules/core/app/shared/lib";
-import { DEFAULT_LANGUAGE } from "@modules/core/i18n/shared/config";
 import { setLanguage } from "@modules/core/i18n/shared/lib";
 import { put, takeEvery } from "redux-saga/effects";
 
-function* worker({ payload }: ReturnType<typeof setLanguage>) {
-	const language = payload ?? DEFAULT_LANGUAGE;
+function* worker({ payload: language }: ReturnType<typeof setLanguage>) {
+	if (!language) {
+		return;
+	}
 
 	yield put(
 		updateAppData({
