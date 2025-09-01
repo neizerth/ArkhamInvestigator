3dc3b9a10a6a88c5eb3aac34b5c00a331565737e
Vladimir Yazykov <neizerth@gmail.com>
2025-09-01 10:08:23 +0300

    fix: unload boards on resume

--- slices/pages/home/lib/hooks/useResumeGame.ts
+++ slices/pages/home/lib/hooks/useResumeGame.ts
@@ -1,18 +1,18 @@
+import { unloadAllBoards } from "@modules/board/base/shared/lib";
 import { usePage } from "@modules/core/router/shared/lib";
 import { routes } from "@shared/config";
-import {
-	selectHasPreviousGame,
-	useAppSelector,
-	usePageLoader,
-} from "@shared/lib";
+import { useAppDispatch, usePageLoader } from "@shared/lib";
+import { useCallback } from "react";
 
 export const useResumeGame = () => {
+	const dispatch = useAppDispatch();
 	const goToPage = usePage();
 	const resume = goToPage(routes.board);
 
-	const hasPreviousGame = useAppSelector(selectHasPreviousGame);
-
 	const [onResume] = usePageLoader(resume);
 
-	return hasPreviousGame && onResume;
+	return useCallback(() => {
+		dispatch(unloadAllBoards());
+		onResume();
+	}, [dispatch, onResume]);
 };
