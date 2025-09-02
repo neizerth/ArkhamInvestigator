bb0a3c6bf32bc75733c4665786af68ade73646a8
Vladimir Yazykov <neizerth@gmail.com>
2025-09-02 10:16:37 +0300

    refactor: progress

--- slices/modules/chaos-bag/reveal/base/entities/ui/ChaosTokenRevealLoader/ChaosTokenRevealLoader.components.tsx
+++ slices/modules/chaos-bag/reveal/base/entities/ui/ChaosTokenRevealLoader/ChaosTokenRevealLoader.components.tsx
@@ -1,21 +1,21 @@
+import {
+	RadialProgressMemo as RadialProgress,
+	type RadialProgressProps,
+} from "@shared/ui";
 import type { FC } from "react";
 import Animated from "react-native-reanimated";
 import styled from "styled-components/native";
-import {
-	ChaosTokenRevealProgress,
-	type ChaosTokenRevealProgressProps,
-} from "../ChaosTokenRevealProgress";
 
 export const Container: typeof Animated.View = styled(Animated.View)`
   justify-content: center;
   align-items: center;
 `;
 
-type ProgressProps = ChaosTokenRevealProgressProps & {
+type ProgressProps = RadialProgressProps & {
 	size: number;
 };
 
-export const Progress: FC<ProgressProps> = styled(ChaosTokenRevealProgress)`
+export const Progress: FC<ProgressProps> = styled(RadialProgress)`
   border-radius: ${({ size }: ProgressProps) => size}px;
   background: rgba(0, 0, 0, 0.1);
 `;
