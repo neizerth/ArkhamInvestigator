17153866258561ac1b5c3949df50fe444fd5fd2b
Vladimir Yazykov <neizerth@gmail.com>
2025-09-04 09:14:06 +0300

    fix: android specific navbar style

--- slices/modules/core/device/entities/navigation-bar/setNavigationBarStyle/setNavigationBarStyleSaga.ts
+++ slices/modules/core/device/entities/navigation-bar/setNavigationBarStyle/setNavigationBarStyleSaga.ts
@@ -1,8 +1,12 @@
 import { setStyle } from "expo-navigation-bar";
+import { Platform } from "react-native";
 import { call, takeEvery } from "redux-saga/effects";
 import { setNavigationBarStyle } from "./setNavigationBarStyle";
 
 function* worker({ payload }: ReturnType<typeof setNavigationBarStyle>) {
+	if (Platform.OS !== "android") {
+		return;
+	}
 	yield call(setStyle, payload);
 }
 
