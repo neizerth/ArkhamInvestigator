591711328c43e353801af7cdb91f188b6468a5ab
Vladimir Yazykov <neizerth@gmail.com>
2025-09-07 08:46:41 +0300

    fix: app loading

--- assets/fonts/zh/FangSong/index.ts
+++ assets/fonts/zh/FangSong/index.ts
@@ -1,5 +1,5 @@
-export const FangSongRegular = require("./FangSong-Regular.woff2");
-export const FangSongItalic = require("./FangSong-Italic.woff2");
+export const FangSongRegular = require("./FangSong-Regular.ttf");
+export const FangSongItalic = require("./FangSong-Italic.ttf");
 
 export const FangSong = {
 	regular: "FangSongRegular",
