4a39afdf32324234170779545176219cdd7f0cfe
Vladimir Yazykov <neizerth@gmail.com>
2025-10-02 14:32:00 +0300

    feat: android overlay compatibility

--- slices/modules/core/modal/app/ui/ModalProvider/ModalProvider.tsx
+++ slices/modules/core/modal/app/ui/ModalProvider/ModalProvider.tsx
@@ -1,9 +1,12 @@
-import type { PropsWithChildren } from "react";
+import { Fragment, type PropsWithChildren } from "react";
 import { FullWindowOverlay } from "react-native-screens";
 import * as C from "./ModalProvider.components";
 
+import { Platform } from "react-native";
 import { useModalBackButton } from "../../../shared/base/lib";
 import { CustomModals } from "../CustomModals";
+
+const Content = Platform.OS === "ios" ? FullWindowOverlay : Fragment;
 
 export const ModalProvider = ({ children }: PropsWithChildren) => {
 	useModalBackButton();
@@ -11,10 +14,10 @@ export const ModalProvider = ({ children }: PropsWithChildren) => {
 	return (
 		<>
 			{children}
-			<FullWindowOverlay>
+			<Content>
 				<C.Modal />
 				<CustomModals />
-			</FullWindowOverlay>
+			</Content>
 		</>
 	);
 };
