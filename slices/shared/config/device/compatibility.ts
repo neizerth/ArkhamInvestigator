ca8405cf6a85faf7e2686adcb7f9c87c156d0446
Vladimir Yazykov <neizerth@gmail.com>
2025-10-03 12:42:35 +0300

    fix: ios start screen image rendering

--- slices/shared/config/device/compatibility.ts
+++ slices/shared/config/device/compatibility.ts
@@ -1,6 +1,6 @@
 import { Platform } from "react-native";
 
-const isAndroid7 =
+const IS_ANDROID_7 =
 	Platform.OS === "android" && [24, 25].includes(Platform.Version);
 
-export const REMOVE_CLIPPED_SUBVIEWS = !isAndroid7;
+export const REMOVE_CLIPPED_SUBVIEWS = !IS_ANDROID_7;
