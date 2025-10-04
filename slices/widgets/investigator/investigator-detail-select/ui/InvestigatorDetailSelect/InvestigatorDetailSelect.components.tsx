288164e176ac768a6f4795a4f9b939da496e5bfe
Vladimir Yazykov <neizerth@gmail.com>
2025-10-04 07:56:24 +0300

    fix: description vertical position

--- slices/widgets/investigator/investigator-detail-select/ui/InvestigatorDetailSelect/InvestigatorDetailSelect.components.tsx
+++ slices/widgets/investigator/investigator-detail-select/ui/InvestigatorDetailSelect/InvestigatorDetailSelect.components.tsx
@@ -21,10 +21,10 @@ export const Content: typeof View = styled(View)`
   flex: 1;
   position: relative;
   z-index: 2;
-  justify-content: center;
+  justify-content: flex-end;
   width: 100%;
   max-width: 500px;
-  padding: ${paddingBlock}px ${size.gap.default}px;
+  padding: ${paddingBlock}px ${size.gap.default}px 50px;
 `;
 
 export const Sections: typeof View = styled(View).attrs({
