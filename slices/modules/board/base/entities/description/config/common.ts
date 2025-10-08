c3c6010fdec5b3cc81d28fcef4af31cb59008b72
Vladimir Yazykov <neizerth@gmail.com>
2025-10-08 14:16:27 +0300

    fix: default board description footer on small screens

--- slices/modules/board/base/entities/description/config/common.ts
+++ slices/modules/board/base/entities/description/config/common.ts
@@ -1,9 +1,14 @@
+import { navBarHeight } from "@shared/config";
 import { Dimensions } from "react-native";
 
 const screen = Dimensions.get("screen");
 
 const largeScreen = screen.height > 640;
 
-export const DEFAULT_PORTRAIT_DESCRIPTION_HEIGHT = largeScreen ? 70 : 80;
+export const DEFAULT_PORTRAIT_DESCRIPTION_HEIGHT = largeScreen
+	? 70
+	: navBarHeight > 0
+		? 80
+		: 60;
 
 export const DESCRIPTION_TEXT_UNIT_SIZE = (screen.width * 6) / 100;
