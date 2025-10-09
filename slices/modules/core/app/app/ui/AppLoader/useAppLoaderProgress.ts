fc3ee87f5942ee5d33890e9f66b80f219064991b
Vladimir Yazykov <neizerth@gmail.com>
2025-10-09 15:45:44 +0300

    fix: progress issue

--- slices/modules/core/app/app/ui/AppLoader/useAppLoaderProgress.ts
+++ slices/modules/core/app/app/ui/AppLoader/useAppLoaderProgress.ts
@@ -4,27 +4,31 @@ import { useAssetImageProgress } from "@modules/core/assets/base/shared/lib/hook
 import { useAppSelector } from "@shared/lib";
 import { useEffect, useState } from "react";
 
-export const useAppLoaderProgress = () => {
-	const { round } = Math;
+const getProgress = (local: number, external: number) => {
+	return Math.round(0.2 * local + 0.8 * external);
+};
 
+export const useAppLoaderProgress = () => {
 	const local = useAssetImageProgress();
 	const external = useAssetDownloadProgress();
 
 	const externalImagesReady = useAppSelector(selectExternalImagesReady);
 	const [imagesReady, setImagesReady] = useState(externalImagesReady);
+	const [progress, setProgress] = useState(0);
 
 	useEffect(() => {
-		if (external.progress >= 100) {
+		if (external.progress >= 100 && !imagesReady) {
 			setImagesReady(true);
+			setProgress(getProgress(local.progress, external.progress));
 		}
-	}, [external.progress]);
+	}, [external.progress, imagesReady, local.progress]);
 
-	if (imagesReady) {
-		return local.progress;
+	if (!imagesReady) {
+		return getProgress(local.progress, external.progress);
 	}
 
-	const externalProgress =
-		external.progress === 0 && imagesReady ? 100 : external.progress;
+	// Scale local progress from transitionProgress to 100
+	const scaledProgress = progress + (100 - progress) * (local.progress / 100);
 
-	return round(0.2 * local.progress + 0.8 * externalProgress);
+	return Math.round(scaledProgress);
 };
