8efae3612a313a3361a44808c3b254a6d7491a10
Vladimir Yazykov <neizerth@gmail.com>
2025-10-14 10:16:00 +0300

    fix: status check cache

--- slices/modules/core/app/entities/checkAppUpdates/getAppStatusData.ts
+++ slices/modules/core/app/entities/checkAppUpdates/getAppStatusData.ts
@@ -2,6 +2,5 @@ import { dataAPI } from "@shared/api";
 import type { BuildInfo } from "arkham-investigator-data";
 import { v4 } from "uuid";
 
-const path = `/status.json?v=${v4()}`;
-
-export const getAppStatusData = () => dataAPI.get<BuildInfo>(path);
+export const getAppStatusData = () =>
+	dataAPI.get<BuildInfo>(`/status.json?v=${v4()}`);
