2018a28853bd2e190070d07935b84b9d4061b21c
Vladimir Yazykov <neizerth@gmail.com>
2025-11-12 03:16:56 +0800

    fix: yuki

--- slices/modules/mechanics/board/abilities/features/base/lib/store/features/investigators/custom/zgoo/YukiYagami/sagas/index.ts
+++ slices/modules/mechanics/board/abilities/features/base/lib/store/features/investigators/custom/zgoo/YukiYagami/sagas/index.ts
@@ -1,8 +1,10 @@
 import { fork } from "redux-saga/effects";
 import { YukiYagamiOpenModalSaga } from "./openModalSaga";
 import { YukiYagamiProcessModalActionSaga } from "./processModalActionSaga";
+import { YukiYagamiReturnToModalSaga } from "./returnToRevealModalSaga";
 
 export function* YukiYagamiAbilitySaga() {
+	yield fork(YukiYagamiReturnToModalSaga);
 	yield fork(YukiYagamiOpenModalSaga);
 	yield fork(YukiYagamiProcessModalActionSaga);
 }
