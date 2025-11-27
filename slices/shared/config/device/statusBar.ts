826beceab91eca7f13c11035515a0ee4edf05163
Vladimir Yazykov <neizerth@gmail.com>
2025-11-27 13:44:24 +0800

    fix: ios status bar height

--- slices/shared/config/device/statusBar.ts
+++ slices/shared/config/device/statusBar.ts
@@ -1,20 +1,15 @@
+import Constants from "expo-constants";
 import { Platform, StatusBar } from "react-native";
-
-import { getStatusBarHeight as getHeight } from "rn-statusbar-height";
-import { IOS_WITH_GESTURE_CONTROL } from "./iphone";
 
 export const DEFAULT_STATUS_BAR_HEIGHT = Platform.OS === "web" ? 10 : 40;
 
 export const statusBarHeight = (() => {
+	if (Platform.OS === "web") {
+		return 10;
+	}
 	if (Platform.OS === "android") {
 		return StatusBar.currentHeight || DEFAULT_STATUS_BAR_HEIGHT;
 	}
 
-	const height = getHeight();
-
-	if (!IOS_WITH_GESTURE_CONTROL) {
-		return height;
-	}
-
-	return height <= 20 ? 54 : height;
+	return Constants.statusBarHeight;
 })();
