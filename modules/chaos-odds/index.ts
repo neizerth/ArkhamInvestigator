e6955d4676325b33238ada888f691719b8efb619
Vladimir Yazykov <neizerth@gmail.com>
2025-12-05 13:19:19 +0300

    refactor: index

--- modules/chaos-odds/index.ts
+++ modules/chaos-odds/index.ts
@@ -1,13 +1,3 @@
-import { NativeModules, Platform } from "react-native";
-
-// Ensure the ChaosOddsJSI bridge module loads on native platforms
-// This module installs the JSI bindings into global.ChaosOdds
-if (Platform.OS !== "web") {
-	// Reference the module to trigger its initialization
-	// The module's setBridge method will install the JSI bindings
-	const _ = NativeModules.ChaosOddsJSI;
-}
-
 // Reexport the native module. On web, it will be resolved to ChaosOddsModule.web.ts
 // and on native platforms to ChaosOddsModule.ts
 export { default } from "./src/ChaosOddsModule";
