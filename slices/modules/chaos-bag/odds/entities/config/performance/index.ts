03f2a494e5598704444b6da27d4ec84fa0e0eb63
Vladimir Yazykov <neizerth@gmail.com>
2025-12-25 10:59:53 +0300

    feat: reveal performance test

--- slices/modules/chaos-bag/odds/entities/config/performance/index.ts
+++ slices/modules/chaos-bag/odds/entities/config/performance/index.ts
@@ -2,6 +2,7 @@ import { coreSetEndTest } from "./01-core-set-end";
 import { innsmouthEndTest } from "./02-innsmouth-end";
 import { edgeFullRevealTest } from "./03-edge-full-reveal";
 import { edgeMoonRevealTest } from "./03-edge-moon-reveal";
+import { edgeScenarioRevealTest } from "./03-edge-scenario-reveal";
 import { strangeMoonsTest } from "./04-strange-moons";
 import { strangeMoonsPerformanceTestGroups } from "./05-strange-moons";
 import { fullLoadTest } from "./06-full-load";
@@ -10,6 +11,7 @@ export const chaosOddsPerformanceTestGroups = [
 	coreSetEndTest,
 	innsmouthEndTest,
 	edgeFullRevealTest,
+	edgeScenarioRevealTest,
 	edgeMoonRevealTest,
 	...strangeMoonsPerformanceTestGroups,
 	strangeMoonsTest,
