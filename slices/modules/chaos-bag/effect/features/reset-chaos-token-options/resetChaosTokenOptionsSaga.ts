3bb8e066c30041c68acf3cb5e56005ddffd6cd68
Vladimir Yazykov <neizerth@gmail.com>
2026-01-06 17:52:58 +0300

    fix: constant option checks

--- slices/modules/chaos-bag/effect/features/reset-chaos-token-options/resetChaosTokenOptionsSaga.ts
+++ slices/modules/chaos-bag/effect/features/reset-chaos-token-options/resetChaosTokenOptionsSaga.ts
@@ -2,6 +2,11 @@ import {
 	setBoardChaosTokenOptions,
 	setChaosTokenOptions,
 } from "@modules/chaos-bag/effect/shared/lib";
+import {
+	setReferenceCardCode,
+	setStoryCode,
+	setStoryDifficultyId,
+} from "@modules/stories/shared/lib";
 import { newGameStarted } from "@shared/lib/store/features/game/actions/startNewGame";
 import { put, takeEvery } from "redux-saga/effects";
 
@@ -10,6 +15,9 @@ function* worker() {
 	yield put(setChaosTokenOptions(null));
 }
 
-export function* resetChaosTokenOptionsOnNewGameSaga() {
+export function* resetChaosTokenOptionsSaga() {
 	yield takeEvery(newGameStarted.match, worker);
+	yield takeEvery(setReferenceCardCode.match, worker);
+	yield takeEvery(setStoryCode.match, worker);
+	yield takeEvery(setStoryDifficultyId.match, worker);
 }
