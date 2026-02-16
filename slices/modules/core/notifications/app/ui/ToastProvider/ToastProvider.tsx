d851f848d35f1635729fc4dcb990fc22856133c2
Vladimir Yazykov <neizerth@gmail.com>
2026-02-16 13:19:10 +0100

    fix: toast position

--- slices/modules/core/notifications/app/ui/ToastProvider/ToastProvider.tsx
+++ slices/modules/core/notifications/app/ui/ToastProvider/ToastProvider.tsx
@@ -1,4 +1,6 @@
+import { selectModalId } from "@modules/core/modal/shared/base/lib";
 import { size, statusBarHeight } from "@shared/config";
+import { useAppSelector } from "@shared/lib";
 import { Fragment, type PropsWithChildren } from "react";
 import { Platform } from "react-native";
 import { FullWindowOverlay } from "react-native-screens";
@@ -8,11 +10,17 @@ import { toastConfig } from "../../config";
 const topOffset = statusBarHeight + size.gap.default;
 const Content = Platform.OS === "ios" ? FullWindowOverlay : Fragment;
 
+const defaultId = "toast-content";
+
 export const ToastProvider = ({ children }: PropsWithChildren) => {
+	const dynamicId = useAppSelector(selectModalId);
+
+	const id = Platform.OS === "ios" ? (dynamicId ?? defaultId) : defaultId;
+
 	return (
 		<>
 			{children}
-			<Content>
+			<Content key={id}>
 				<Toast config={toastConfig} topOffset={topOffset} />
 			</Content>
 		</>
