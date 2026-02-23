67d95c462d3c5a16add8239cab77c8d3bfc42619
Vladimir Yazykov <neizerth@gmail.com>
2026-02-23 18:20:10 +0100

    fix: isabelle

--- slices/modules/mechanics/board/abilities/features/base/lib/store/features/investigators/campaigns/core2026/IsabelleBarnes/elderSignSaga.ts
+++ slices/modules/mechanics/board/abilities/features/base/lib/store/features/investigators/campaigns/core2026/IsabelleBarnes/elderSignSaga.ts
@@ -2,6 +2,7 @@ import { healHorror } from "@modules/board/base/entities/base/lib";
 import { selectBoardById } from "@modules/board/base/shared/lib";
 import type { chaosBagRevealEnd } from "@modules/chaos-bag/reveal/base/entities/lib";
 import { filterElderSignSuccess } from "@modules/chaos-bag/reveal/base/entities/lib/store/features/endChaosBagReveal/filterElderSignSuccess";
+import { getBoardHorror } from "@modules/mechanics/board/base/entities/lib";
 import { InvesigatorCode } from "@modules/mechanics/investigator/entities/config";
 import { put, select, takeEvery } from "redux-saga/effects";
 
@@ -18,6 +19,12 @@ function* worker({ payload }: ReturnType<typeof chaosBagRevealEnd>) {
 	const board: ReturnType<typeof boardSelector> = yield select(boardSelector);
 
 	if (board.investigator.code !== InvesigatorCode.IsabelleBarnes.core2026) {
+		return;
+	}
+
+	const horror = getBoardHorror(board);
+
+	if (horror === 0) {
 		return;
 	}
 
