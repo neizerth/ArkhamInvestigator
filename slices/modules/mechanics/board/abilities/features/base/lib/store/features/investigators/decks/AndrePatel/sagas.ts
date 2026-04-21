5a9247e136f857e4a076099b80b077919745e585
Vladimir Yazykov <neizerth@gmail.com>
2026-04-21 10:42:08 +0300

    feat: andre patel elder sign saga

--- slices/modules/mechanics/board/abilities/features/base/lib/store/features/investigators/decks/AndrePatel/sagas.ts
+++ slices/modules/mechanics/board/abilities/features/base/lib/store/features/investigators/decks/AndrePatel/sagas.ts
@@ -1,6 +1,8 @@
 import { fork } from "redux-saga/effects";
+import { AndrePatelElderSignSaga } from "./elderSignSaga";
 import { AndrePatelSucceedSaga } from "./succeedSaga";
 
 export function* AndrePatelAbilitySaga() {
 	yield fork(AndrePatelSucceedSaga);
+	yield fork(AndrePatelElderSignSaga);
 }
