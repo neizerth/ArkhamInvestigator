c842cbc394902d4c8c664d725a292d1f9186847d
Vladimir Yazykov <neizerth@gmail.com>
2026-05-11 13:51:34 +0300

    fix: fill chaos bag

--- slices/modules/stories/entities/lib/store/features/fillChaosBagDifficulty/fillChaosBagDifficulty.ts
+++ slices/modules/stories/entities/lib/store/features/fillChaosBagDifficulty/fillChaosBagDifficulty.ts
@@ -1,8 +1,13 @@
+import { withRemoteMeta } from "@modules/core/network/shared/lib";
 import { createAction } from "@reduxjs/toolkit";
 
 export type FillChaosBagDifficultyPayload = {
 	difficultyId: string;
 };
 
-export const fillChaosBagDifficulty =
-	createAction<FillChaosBagDifficultyPayload>("stories/fillChaosBagDifficulty");
+export const fillChaosBagDifficulty = createAction(
+	"stories/fillChaosBagDifficulty",
+	withRemoteMeta<FillChaosBagDifficultyPayload>({
+		notify: "all",
+	}),
+);
