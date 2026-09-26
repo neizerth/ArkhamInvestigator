a9287e5734e03d73825ebb2823b7a3fdcad962be
Vladimir Yazykov <neizerth@gmail.com>
2026-09-26 08:46:29 +0300

    refactor: skin from signature

--- slices/modules/signature/base/shared/lib/common/withSignatureGroupChapter.ts
+++ slices/modules/signature/base/shared/lib/common/withSignatureGroupChapter.ts
@@ -1,33 +1,9 @@
 import type {
-	InvestigatorImage,
 	InvestigatorSignature,
 	InvestigatorSignatureGroup,
-	InvestigatorSkin,
 } from "arkham-investigator-data";
 import { isNotNil, partition, uniqBy } from "ramda";
-import { getSignatureOptionName } from "./getSignatureOptionName";
-
-const isCompleteImage = (
-	image: InvestigatorSignature["image"],
-): image is InvestigatorImage & { id: string } => {
-	type Key = keyof InvestigatorImage;
-	const keys: Key[] = ["id", "face", "width", "height"];
-	return keys.every((key) => key in image);
-};
-
-const toSkin = (
-	signature: InvestigatorSignature,
-): InvestigatorSkin | undefined => {
-	if (!isCompleteImage(signature.image)) {
-		return;
-	}
-
-	return {
-		id: signature.id,
-		name: getSignatureOptionName(signature),
-		image: signature.image,
-	};
-};
+import { createSkinFromSignature } from "./createSkinFromSignature";
 
 export const withSignatureGroupChapter =
 	(chapter: number) =>
@@ -40,14 +16,14 @@ export const withSignatureGroupChapter =
 			hasChapter,
 			signatureGroup.signatures,
 		);
-		const excludedSkins = excluded.map(toSkin).filter(isNotNil);
+		const excludedSkins = excluded
+			.map(createSkinFromSignature)
+			.filter(isNotNil);
 		const [firstSignature] = signatures;
 		const { id, code } = firstSignature;
 
-		const skins = uniqBy(
-			(skin) => skin.image.id,
-			[...signatureGroup.skins, ...excludedSkins],
-		);
+		const mergedSkins = [...signatureGroup.skins, ...excludedSkins];
+		const skins = uniqBy((skin) => skin.image.id, mergedSkins);
 
 		return {
 			...signatureGroup,
